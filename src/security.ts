b8913042433c4be37413a5fdbcb48e35760e84c3
Karl-Erik Gustafsson <ke.gustafsson@gmail.com>
2023-10-19 22:20:28 +0300

    Update security.ts

--- src/security.ts
+++ src/security.ts
@@ -356,7 +356,7 @@ export function createCertificateOptions(
 ) {
   const location = app.config.configPath ? app.config.configPath : './settings'
   debug(`Creating certificate files in ${location}`)
-  generate(null, { clientCertificate: true }, function (err, pems) {
+  generate([{ name: 'commonName', value: 'localhost' }], { days: 365 }, function (err, pems) {
     writeFileSync(keyFile, pems.private)
     chmodSync(keyFile, '600')
     writeFileSync(certFile, pems.cert)
