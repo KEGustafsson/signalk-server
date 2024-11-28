a17de7a04981d4e440a6c8789e16d18d7198bbec
Karl-Erik Gustafsson <ke.gustafsson@gmail.com>
2024-11-28 21:35:11 +0200

    fix: react-router

--- packages/server-admin-ui/src/bootstrap.js
+++ packages/server-admin-ui/src/bootstrap.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { HashRouter, Route, Routes } from 'react-router-dom'
+import { HashRouter, Route, Routes } from 'react-router'
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
 import {thunk} from 'redux-thunk'
