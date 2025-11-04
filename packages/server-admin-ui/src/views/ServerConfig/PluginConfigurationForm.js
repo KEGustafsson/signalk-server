38680a5eb7b04f81596f344f7051ed40169a50cd
KE Gustafsson <ke.gustafsson@gmail.com>
2025-11-04 18:55:23 +0200

    feat: add custom button templates and styles for RJSF in PluginConfigurationForm

--- packages/server-admin-ui/src/views/ServerConfig/PluginConfigurationForm.js
+++ packages/server-admin-ui/src/views/ServerConfig/PluginConfigurationForm.js
@@ -5,6 +5,30 @@ import validator from '@rjsf/validator-ajv8'
 
 const Form = withTheme(Bootstrap4Theme)
 
+// Custom button templates to match the original styling
+const customTemplates = {
+  ButtonTemplates: {
+    AddButton: (props) => (
+      <button
+        {...props}
+        type="button"
+        className="btn btn-info btn-add col-xs-3 col-xs-offset-9"
+      >
+        <i className="glyphicon glyphicon-plus" />
+      </button>
+    ),
+    RemoveButton: (props) => (
+      <button
+        {...props}
+        type="button"
+        className="btn btn-danger col-xs-2 array-item-remove"
+      >
+        <i className="glyphicon glyphicon-remove" />
+      </button>
+    )
+  }
+}
+
 // eslint-disable-next-line react/display-name
 export default ({ plugin, onSubmit }) => {
   const schema = JSON.parse(JSON.stringify(plugin.schema))
@@ -37,6 +61,7 @@ export default ({ plugin, onSubmit }) => {
       schema={topSchema}
       uiSchema={uiSchema}
       formData={plugin.data || {}}
+      templates={customTemplates}
       onSubmit={(submitData) => {
         onSubmit({
           ...submitData.formData,
