543a8ab3517c00df775b76a60a5d95cca2338b5d
KE Gustafsson <ke.gustafsson@gmail.com>
2025-11-04 19:12:45 +0200

    feat: enhance button styles and add custom templates for RJSF in PluginConfigurationForm

--- packages/server-admin-ui/src/views/ServerConfig/PluginConfigurationForm.js
+++ packages/server-admin-ui/src/views/ServerConfig/PluginConfigurationForm.js
@@ -5,8 +5,80 @@ import validator from '@rjsf/validator-ajv8'
 
 const Form = withTheme(Bootstrap4Theme)
 
+// Custom ArrayFieldItemTemplate to match the original layout
+const ArrayFieldItemTemplate = (props) => {
+  const {
+    children,
+    disabled,
+    hasToolbar,
+    hasMoveDown,
+    hasMoveUp,
+    hasRemove,
+    index,
+    onDropIndexClick,
+    onReorderClick,
+    readonly,
+    registry,
+    uiSchema
+  } = props
+
+  const { RemoveButton } = registry.templates.ButtonTemplates
+
+  return (
+    <div className="row array-item">
+      <div className="col-xs-12">
+        {children}
+      </div>
+      {hasToolbar && (
+        <div className="col-xs-12">
+          {hasRemove && (
+            <RemoveButton
+              className="array-item-remove"
+              disabled={disabled || readonly}
+              onClick={onDropIndexClick(index)}
+              uiSchema={uiSchema}
+              registry={registry}
+            />
+          )}
+        </div>
+      )}
+    </div>
+  )
+}
+
+// Custom ArrayFieldTemplate to match the original layout
+const ArrayFieldTemplate = (props) => {
+  const { canAdd, items, onAddClick, title, schema } = props
+
+  return (
+    <fieldset className="field field-array field-array-of-object">
+      {title && <legend>{title}</legend>}
+      <div className="row array-item-list">
+        <div className="col-xs-12">
+          {items && items.map((item) => item.children)}
+        </div>
+      </div>
+      {canAdd && (
+        <div className="row">
+          <div className="col-xs-12">
+            <button
+              type="button"
+              className="btn btn-info btn-add col-xs-3 col-xs-offset-9"
+              onClick={onAddClick}
+            >
+              <i className="glyphicon glyphicon-plus" />
+            </button>
+          </div>
+        </div>
+      )}
+    </fieldset>
+  )
+}
+
 // Custom button templates to match the original styling
 const customTemplates = {
+  ArrayFieldTemplate,
+  ArrayFieldItemTemplate,
   ButtonTemplates: {
     AddButton: (props) => (
       <button
@@ -21,7 +93,7 @@ const customTemplates = {
       <button
         {...props}
         type="button"
-        className="btn btn-danger col-xs-2 array-item-remove"
+        className="btn btn-danger array-item-remove"
       >
         <i className="glyphicon glyphicon-remove" />
       </button>
